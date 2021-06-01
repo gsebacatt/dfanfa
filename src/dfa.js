98292cf726c47706d234b4652bf1531e7f440eed
Sebastian Cattaneo <gsebacatt@gmail.com>
2021-06-01 19:22:05 -0400

    Updated mover function

--- src/dfa.js
+++ src/dfa.js
@@ -2,67 +2,100 @@ module.exports = {
     toDfa,
     getEClosure,
     minimize,
+    mover
 }
 
 
 //Tener definido el lenguaje
-const L = ['a','b'];
+const L = ['a', 'b', 'c'];
 
 
-function Minimize(dfa){
+function Minimize(dfa) {
 
 }
 
+function createState(isEnd) {
+    return {
+        isEnd,
+        //Nfa states represent it's composition
+        states: new Set(),
+        marked: false,
+        //Puede tener mas de una transicion
+        transitions: {},
+    };
+}
 
-//Subconjuntos
-function toDfa(nfa){
+function addTransition(from, to, symbol) {
+    from.transition[symbol] = to;
+}
+
+function fromSymbol(symbol) {
+    const start = createState(false);
+    const end = createState(true);
+    addTransition(start, end, symbol);
+
+    return {start, end};
+}
+
+function toDfa(nfa) {
     console.log(nfa)
     //Recordar que el nfa es un stack en un array
-   let dfa = new Set();
+    let dfa = new Set();
 
     dfa.add(getEclosure(nfa.start));
 
     dfa.forEach(state => {
         console.log(state)
-        if(!state.marked){
-           state.marked = true;
+        if (!state.marked) {
+            state.marked = true;
 
 
-           L.forEach(symbol => {
-               let U  = getEClosure(mover(dfa,state, symbol));
+            L.forEach(symbol => {
+                let T = mover(state, symbol);
 
-               if(!dfa.has(U)){
-                   U.marked = true;
-                   dfa.add(U);
-               }
-           })
+                let U = new Set();
+
+                T.forEach(t => {
+                    U.add(getEClosure(t));
+                })
+
+                if (!dfa.has(U)) {
+                    U.marked = true;
+                    dfa.add(U);
+                }
+            })
 
         }
     })
 }
 
+function mover(state, symbol) {
+    let tempSet = new Set();
 
+    console.log(state.transition)
 
-//Para que eclosure funcione, mover deberia retornar un estado
-function mover(dfa, state,symbol){
-   let tempSet = new Set(...dfa[state], symbol);
-   return tempSet;
+    for (const [key, value] in state.transition) {
+        if(key === symbol){
+            tempSet.add(value)
+        }
+    }
+    return tempSet;
 }
 
 
-function getEClosure(initialState){
-    let closure  = new Set();
+function getEClosure(initialState) {
+    let closure = new Set();
 
     let neighbours = initialState.epsilonTransitions;
 
     neighbours.forEach(neighbour => {
-            closure.add(getEClosure(neighbour))
+        closure.add(getEClosure(neighbour))
     })
 
     return closure;
 
 }
 
-function minimize(dfa){
+function minimize(dfa) {
     return [];
 }
